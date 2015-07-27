24b88e6394c405959ba57acd45bd028be4e7bd97
Cory Spitzer <coryspitzer@gmail.com>
2015-07-27 12:30:09 -0700

    Get an answer using an OO approach

--- pe1.py
+++ pe1.py
@@ -1,8 +1,9 @@
-from pe1_library import *
+from pe1_library import find_sum, Multiples
 
 def main():
     # print('main called')
     print('Functionally we get: ' + str(find_sum()))
+    print('Objectively we get: ' + str(Multiples(3,5,1000).sum()))
 
 # only execute main() if the script is being run directly, but not imported;
 # __name__ is set to '__main__' only if we run 'python pe1.py',
--- pe1_library.py
+++ pe1_library.py
@@ -6,3 +6,16 @@ def find_sum(interval1 = 3, interval2 = 5, limit = 1000):
         if i % interval1 == 0 or i % interval2 == 0:
             sum += i
     return sum
+
+class Multiples:
+    def __init__(self, interval1, interval2, limit):
+        self.interval1 = interval1
+        self.interval2 = interval2
+        self.limit = limit
+        self.total = 0
+
+    def sum(self):
+        for i in range(self.interval1, self.limit):
+            if i % self.interval1 == 0 or i % self.interval2 == 0:
+                self.total += i
+        return self.total
